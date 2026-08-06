ec81aab68db9f6883e461f924a9c2eee7c7fe797
hieunguyennho <hieunho500@gmail.com>
2026-08-06 22:43:05 +0700

    src code BE

---
+++ src/main/java/n1h5/models/N1h5ModelsBackendApplication.java
@@ -0,0 +1,13 @@
+package n1h5.models;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N1h5ModelsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N1h5ModelsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/n1h5/models/N1h5ModelsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package n1h5.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N1h5ModelsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
